fec61d532a5cc9592738010eeecc2b7f1ccd30b1
yuukinht <50871323+yuukinht@users.noreply.github.com>
2020-01-12 12:24:51 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var app = express();
+//var server = app.listen(process.env.PORT || 3000, listen);
+var server = app.listen(3000);
+app.use(express.static('public'));
+var io = require('socket.io')(server);
+var players = {};
+var bullets = {};
+var socketlist = {};
+var wWidth = 810;
+var wHeight = 540;
+io.sockets.on('connection',
+  function (socket) { 
+	players[socket.id] = new Player();
+	socketlist[socket.id] = socket;
+	socket.on('setinfo', function(data) {		
+		players[socket.id].name = data.name;		
+		players[socket.id].color = data.color;		
+	});
+	socket.on('move', function(data) {
+		
+		var px = players[socket.id].x;
+		var py = players[socket.id].y;
+		var x = data.x*10;
+		var y = data.y*10;
+		if ((px+x>-1)&&(px+x<wWidth-29)){
+			var kt = true;
+			for(var i in players){
+				if (((Math.abs(px+x-players[i].x)<30)&&(Math.abs(py+y-players[i].y)<30))&&(i !== socket.id)){
+					kt = false;
+				}
+			}
+			if (kt == true){players[socket.id].x += data.x*10;}
+		}
+		if ((py+y>-1)&&(py+y<wHeight-29)){
+			var kt = true;
+			for(var i in players){
+				if (((Math.abs(px+x-players[i].x)<30)&&(Math.abs(py+y-players[i].y)<30))&&(i !== socket.id)){
+					kt = false;
+				}
+			}			
+			if (kt == true){players[socket.id].y += data.y*10;}
+		}		
+    });
+	socket.on('changedir', function(data) {
+		players[socket.id].dir = data.dir;
+    });	
+    socket.on('disconnect', function() {
+		delete players[socket.id];
+    });	
+  }
+);
+setInterval(function(){
+	for(var i in socketlist){
+		socketlist[i].emit('sceneupdate',players);
+	}
+},1000/30);
+function Player(){
+	var pl = {
+		name:"Player",
+		x:0,
+		y:0,
+		dir:0,
+		color:0
+	}
+	return pl;
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+
---
+++ public/particles.js
@@ -0,0 +1,84 @@
+var canvas;
+var players = {};
+var bullets = {};
+var test;
+var socket;
+var dir,mx,my;
+function setup() {
+	dir = 0;
+    canvas = createCanvas(810, 540);
+    canvas.parent('mainCanvas');
+    frameRate(30);
+	strokeWeight(1);
+	stroke(0);
+	//socket = io.connect('http://localhost:3000');
+	socket = io.();
+	socket.emit('setinfo',{name:"Yuuki",color:255});
+	
+	socket.on('sceneupdate', function(data) {
+		players = data;
+		dir = players[socket.id].dir;
+    });	
+}
+function draw() {
+	clear();
+	sceneupdate();
+}
+
+function sceneupdate(){
+	for (var i in players){
+		var tank = new Tank(players[i]);
+		tank.draw();
+	}
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+function Tank(obj){
+	this.co = obj.color;
+	this.x = obj.x;
+	this.y = obj.y;
+	this.name = obj.name;
+	this.dir = obj.dir;
+}
+Tank.prototype.draw = function(){
+	fill(this.co);	
+	switch (this.dir){
+		case 0 :
+			beginShape();vertex(this.x+10, this.y);vertex(this.x+20, this.y);vertex(this.x+20, this.y+10);vertex(this.x+30, this.y+10);vertex(this.x+30, this.y+30);vertex(this.x, this.y+30);vertex(this.x, this.y+10);vertex(this.x+10, this.y+10);endShape(CLOSE);break;
+		case 1 :
+			beginShape();vertex(this.x, this.y);vertex(this.x+20, this.y);vertex(this.x+20, this.y+10);vertex(this.x+30, this.y+10);vertex(this.x+30, this.y+20);vertex(this.x+20, this.y+20);vertex(this.x+20, this.y+30);vertex(this.x, this.y+30);endShape(CLOSE);break;
+		case 2 :
+			beginShape();vertex(this.x, this.y);vertex(this.x+30, this.y);vertex(this.x+30, this.y+20);vertex(this.x+20, this.y+20);vertex(this.x+20, this.y+30);vertex(this.x+10, this.y+30);vertex(this.x+10, this.y+20);vertex(this.x, this.y+20);endShape(CLOSE);break;
+		case 3 :
+			beginShape();vertex(this.x+10, this.y);vertex(this.x+30, this.y);vertex(this.x+30, this.y+30);vertex(this.x+10, this.y+30);vertex(this.x+10, this.y+20);vertex(this.x, this.y+20);vertex(this.x, this.y+10);vertex(this.x+10, this.y+10);endShape(CLOSE);break;			
+	}	
+}
+window.addEventListener("keydown", function (e) {
+	if (e.which === 38) {
+		if (dir == 0){	
+			socket.emit('move',{x:0,y:-1});
+		}else{			
+			socket.emit('changedir',{dir:0});
+		}
+	} else if (e.which === 39) {
+		if (dir === 1){
+			socket.emit('move',{x:1,y:0});
+		}else{
+			socket.emit('changedir',{dir:1});
+		}
+	} else if (e.which === 40) {
+		if (dir === 2){
+			socket.emit('move',{x:0,y:1});
+		}else{
+			socket.emit('changedir',{dir:2});
+		}
+	} else if (e.which === 37) {
+		if (dir === 3){
+			socket.emit('move',{x:-1,y:0});
+		}else{
+			socket.emit('changedir',{dir:3});
+		}
+	}
+});
